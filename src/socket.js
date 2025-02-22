2dcc95e36afa1f7b1d61277de7dadf66aad7cf9a
Chandler Casey <chandlercasey13@gmail.com>
2025-02-21 19:29:42 -0600

    Change socket url

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,6 @@
 "use client"
 import { io } from "socket.io-client";
 
-// ✅ Use WebSocket URL from environment variable
-const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3000";
 
-export const socket = io(SOCKET_URL);
+
+export const socket = io();
